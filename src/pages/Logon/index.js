5c0d0eb9babe3e1dbb45ba5c1b9c1e6803dda15b
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-03-25 13:42:11 -0300

    Adiciona página de logon

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,31 @@
+import React from 'react' 
+import { FiLogIn } from 'react-icons/fi'
+
+import './style.css'
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
+
+export default function Logon(){
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                <form>
+                    <h1> Faça seu Logon</h1>
+
+                    <input placeholder="Sua ID" />
+                    <button type="submit" className="button">Entrar</button>  
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#e02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+
+
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
+    )
+}
